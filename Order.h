6de5dac192b9453a8d4928e8c5a62524752f9879
Aditya Kshirsagar <kaditya.anil@gmail.com>
2026-01-24 01:56:16 +0530

    init

---
+++ Order.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <cstdint>
+#include <memory>
+
+enum class Side
+{
+    SIDE_BUY,
+    SIDE_SELL
+};
+
+class Order;
+typedef std::shared_ptr<Order> HOrder;
+
+class Order
+{
+public:
+    Order(uint64_t order_id, Side side, double price, uint32_t qty)
+        : order_id(order_id), side(side), price(price), qty(qty), prev(nullptr), next(nullptr)
+    {
+    }
+
+    uint64_t getOrderId() const { return order_id; }
+    Side getSide() const { return side; }
+    double getPrice() const { return price; }
+    uint32_t getQty() const { return qty; }
+
+    void setPrev(HOrder p) { prev = p; }
+    HOrder getPrev() const { return prev; }
+
+    void setNext(HOrder n) { next = n; }
+    HOrder getNext() const { return next; }
+
+private:
+    uint64_t order_id;
+    Side side;
+    double price;
+    uint32_t qty;
+
+    HOrder prev;
+    HOrder next;
+}
